7f2741a43c6fce4538a1d0746e82a1e89560601d
Vardaan <vardaan13121997@gmail.com>
2018-04-28 21:22:05 +0530

    Initial commit

---
+++ app/src/main/java/com/vardaan/activityrecognition/Constants.java
@@ -0,0 +1,12 @@
+package com.vardaan.activityrecognition;
+
+/**
+ * Created by Vardaan on 28-04-2018.
+ */
+
+public class Constants {
+
+    public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 30 * 1000;
+    public static final int CONFIDENCE = 70;
+}
---
+++ app/src/main/java/com/vardaan/activityrecognition/DetectedActivitiesIntentService.java
@@ -0,0 +1,50 @@
+package com.vardaan.activityrecognition;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Vardaan on 28-04-2018.
+ */
+
+public class DetectedActivitiesIntentService extends IntentService {
+
+    protected static final String TAG = DetectedActivitiesIntentService.class.getSimpleName();
+
+    public DetectedActivitiesIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+
+        ArrayList<DetectedActivity> detectedActivities = (ArrayList) result.getProbableActivities();
+
+        for (DetectedActivity activity : detectedActivities) {
+            Log.e(TAG, "Detected activity: " + activity.getType() + ", " + activity.getConfidence());
+            broadcastActivity(activity);
+        }
+    }
+
+    private void broadcastActivity(DetectedActivity activity) {
+
+        Intent intent = new Intent(Constants.BROADCAST_DETECTED_ACTIVITY);
+        intent.putExtra("type", activity.getType());
+        intent.putExtra("confidence", activity.getConfidence());
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+}
